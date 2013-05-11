240d3cc4bf7096bac7b0ce89c7e94de69393c818
Laura Morris <laura.ann.morris@gmail.com>
2013-05-11 07:41:08 +0000

    Revert "Should be good now"
    This reverts commit d4e245adef2c32d6a7fdf5bfaf1598fed04ea949.

--- db/migrate/20130507192532_add_google_oauth_to_user.rb
+++
@@ -1,7 +0,0 @@
-class AddGoogleOauthToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :provider, :string
-    add_column :users, :uid, :string
-    add_column :users, :provider_email, :string
-  end
-end
